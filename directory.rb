05aa92fab1afc8376e1692ebab497d11ce2a9a60
KiranKukar <kiranrkukar@gmail.com>
2021-12-01 11:54:35 +0000

    created arrray and refactored

--- directory.rb
+++ directory.rb
@@ -1,17 +1,10 @@
-student_count = 11
+students = ["Dr. Hannibal Lecter",
+    "Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex DeLarge",
+    "The Wicked Witch of the West", "Terminator", "Freddy Krueger",
+    "The Joker", "Joffrey Baratheon", "Norman Bates"]
 # first we print the header and the list of students on seperate lines
 puts "The students of Villains Academyy"
 puts "-----------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+puts students[0..-1]
 # Then we print the count of students on one line
-print "Overall, we have #{student_count} great students"
+print "Overall, we have #{students.count} great students"
