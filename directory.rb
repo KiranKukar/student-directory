d9d22d5198f694409d88c52316f4265506657810
KiranKukar <kiranrkukar@gmail.com>
2021-12-01 13:00:13 +0000

    added methods and called them

--- directory.rb
+++ directory.rb
@@ -2,9 +2,23 @@ students = ["Dr. Hannibal Lecter",
     "Darth Vader", "Nurse Ratched", "Michael Corleone", "Alex DeLarge",
     "The Wicked Witch of the West", "Terminator", "Freddy Krueger",
     "The Joker", "Joffrey Baratheon", "Norman Bates"]
-# first we print the header and the list of students on seperate lines
-puts "The students of Villains Academyy"
-puts "-----------"
-puts students[0..-1]
-# Then we print the count of students on one line
-print "Overall, we have #{students.count} great students"
+# methods to print the header, the list of students on seperate lines and the student count
+def print_header
+    puts "The students of Villains Academy"
+    puts "-----------"
+end
+
+def print_list(names)
+    puts names[0..-1]
+    puts "yes"
+end 
+
+def print_footer(names)
+    print "Overall, we have #{names.count} great students"
+end
+
+# Call the methods so the programme actually runs
+
+print_header
+print_list(students)
+print_footer(students)
